8005df9f34979aba51937dd05fb0e28a99492b82
Tal Adivi <39825719+TalAdivi@users.noreply.github.com>
2021-03-25 16:19:46 +0200

    update reward function

--- Reward function/source.py
+++ Reward function/source.py
@@ -7,26 +7,26 @@ def reward_function(params):
     all_wheels_on_track = params['all_wheels_on_track']         # flag to indicate if the agent is on the track
     x = params['x']                                             # agent's x-coordinate in meters
     y = params['y']                                             # agent's y-coordinate in meters
-    closest_objects = params['closest_objects']                 # zero-based indices of the two closest objects to the agent's current position of (x y).
+    closest_objects = params['closest_objects']                 # zero-based indices of the two closest objects to the agent's 
     closest_waypoints = params['closest_waypoints']             # indices of the two nearest waypoints.
     distance_from_center = params['distance_from_center']       # distance in meters from the track center 
     is_crashed = params['is_crashed']                           # Boolean flag to indicate whether the agent has crashed.
-    is_left_of_center = params['is_left_of_center']             # Flag to indicate if the agent is on the left side to the track center or not. 
-    is_offtrack = params['is_offtrack']                         # Boolean flag to indicate whether the agent has gone off track.
-    is_reversed = params['is_reversed']                         # flag to indicate if the agent is driving clockwise (True) or counter clockwise (False).
+    is_left_of_center = params['is_left_of_center']             # Flag to indicate if the agent is on the left side to the
+    is_offtrack = params['is_offtrack']                         # Boolean flag to indicate whether the agent has gone off track
+    is_reversed = params['is_reversed']                         # flag to indicate if the agent is driving clockwise (True) or 
     heading = params['heading']                                 # agent's yaw in degrees
-    objects_distance = params['objects_distance']               # list of the objects' distances in meters between 0 and track_length in relation to the starting line.
-    objects_heading = params['objects_heading']                 # list of the objects' headings in degrees between -180 and 180.
-    objects_left_of_center = params['objects_left_of_center']   # list of Boolean flags indicating whether elements' objects are left of the center (True) or not (False).
-    objects_locationfloat = params['objects_locationfloat']     # list of object locations [(x,y) ...].
+    objects_distance = params['objects_distance']               # list of the objects' distances in meters between 0 and 
+    objects_heading = params['objects_heading']                 # list of the objects' headings in degrees between -180 and 180
+    objects_left_of_center = params['objects_left_of_center']   # list of Boolean flags indicating whether elements' objects 
+    objects_location = params['objects_location']     # list of object locations [(x,y) ...].
     objects_speed = params['objects_speed']                     # list of the objects' speeds in meters per second.
     progress = params['progress']                               # percentage of track completed
     speed = params['speed']                                     # agent's speed in meters per second (m/s)
-    steering_angle = params['steering_angle']                   # agent's steering angle in degrees
+    steering_angle = abs(params['steering_angle'])                   # agent's steering angle in degrees
     steps = params['steps']                                     # number steps completed
     track_length = params['track_length']                       # track length in meters.
     track_width = params['track_width']                         # width of the track
-    waypointsfloat = params['waypointsfloat']                   # list of (x,y) as milestones along the track center
+    waypoints = params['waypoints']                             # list of (x,y) as milestones along the track center
     
     
     # Read all 155 track coordinates (x,y) after racing lines optimization.
@@ -206,6 +206,9 @@ def reward_function(params):
         reward = 0.1
     else:
         reward = 1e-3  # likely crashed/ close to off track
+    
+    if progress == 100:
+        reward += 100.0
     
     return float(reward)
 
